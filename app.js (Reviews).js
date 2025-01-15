40fa604e305c786501fbea13865f97afc4956e1e
IamRobfr <valmav19@gmail.com>
2025-01-15 12:22:33 -0800

    app.js (Reviews).js

--- app.js (Reviews).js
+++ app.js (Reviews).js
@@ -1,84 +1,84 @@
 // local reviews data
 const reviews = [
-  {
-    id: 1,
-    name: "susan smith",
-    job: "web developer",
-    img: "https://www.course-api.com/images/people/person-1.jpeg",
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
-  {
-    id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    img: "https://www.course-api.com/images/people/person-2.jpeg",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-  },
-  {
-    id: 3,
-    name: "peter jones",
-    job: "intern",
-    img: "https://www.course-api.com/images/people/person-4.jpeg",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-  },
-  {
-    id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    img: "https://www.course-api.com/images/people/person-3.jpeg",
-    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
-  },
-];
-// select items
-const img = document.getElementById("person-img");
-const author = document.getElementById("author");
-const job = document.getElementById("job");
-const info = document.getElementById("info");
-
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-const randomBtn = document.querySelector(".random-btn");
-
-// set starting item
-let currentItem = 0;
-
-// load initial item
-window.addEventListener("DOMContentLoaded", function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-});
-
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-}
-// show next person
-nextBtn.addEventListener("click", function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
+    {
+      id: 1,
+      name: "Chill guy a.k.a. 'My new character'",
+      job: 'Smiling Friends Inc.',
+      img: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c7/Chill_guy_original_artwork.jpg/220px-Chill_guy_original_artwork.jpg',
+      text: "I'm just a chill guy, everyone hears from me. But also a worker at the Smiling friends. Looks great, man. (Note: This is a fictional character that is used as a internet meme and art.)",
+    },
+    {
+      id: 2,
+      name: 'Rick Astley',
+      job: 'English singer',
+      img: 'https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1000&h=667&crop=1',
+      text: 'Never gonna give you up. Never gonna let you down. Never gonna run around and desert you. Never gonna make you cry. Never gonna say goodbye. Never gonna tell a lie and hurt you.',
+    },
+    {
+      id: 3,
+      name: 'Ernest Khalimov',
+      job: 'Bodybuilder',
+      img: 'https://preview.redd.it/a-smiling-picture-of-ernest-khalimov-v0-cjg40dsceria1.jpg?width=640&crop=smart&auto=webp&s=f95f4f7e867f5dbc15b4ba4952d2526f670f9b2a',
+      text: 'Stop calling me Gigachad.',
+    },
+    {
+      id: 4,
+      name: 'Satoru Gojo',
+      job: 'Main character of Jujutsu Kaisen',
+      img: 'https://static.wikia.nocookie.net/jujutsu-kaisen/images/5/5a/Satoru_Gojo_arrives_on_the_battlefield_%28Anime%29.png/revision/latest?cb=20210226205256',
+      text: "Would you lose? Nah, I'd win.",
+    },
+  ];
+  // select items
+  const img = document.getElementById('person-img');
+  const author = document.getElementById('author');
+  const job = document.getElementById('job');
+  const info = document.getElementById('info');
+  
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  const randomBtn = document.querySelector('.random-btn');
+  
+  // set starting item
+  let currentItem = 0;
+  
+  // load initial item
+  window.addEventListener('DOMContentLoaded', function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  });
+  
+  // show person based on item
+  function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
   }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener("click", function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener("click", function () {
-  console.log("hello");
-
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
+  // show next person
+  nextBtn.addEventListener('click', function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+      currentItem = 0;
+    }
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener('click', function () {
+    currentItem--;
+    if (currentItem < 0) {
+      currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+  });
+  // show random person
+  randomBtn.addEventListener('click', function () {
+    console.log('hello');
+  
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson(currentItem);
+  });
